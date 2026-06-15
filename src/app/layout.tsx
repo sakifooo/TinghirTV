8218c24d77740d2abc38660a55ae65e79262610a
sakifooo <saki.sasin@gmail.com>
2026-06-15 13:35:35 +0100

    Enhance metadata for Tinghir TV application
    Updated metadata with additional keywords and descriptions in Arabic.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,8 +3,56 @@ import './globals.css';
 
 export const metadata: Metadata = {
   title: 'Tinghir TV',
-  description: 'Download Tinghir TV to watch live football matches, highlights, and sports news anywhere, anytime.',
+  description: 'Download Tinghir TV to watch live football matches, highlights, and sports news anywhere, anytime.  
+حمّل تطبيق Tinghir TV لمشاهدة مباريات كرة القدم مباشرة، وأهداف المباريات، وآخر الأخبار الرياضية في أي وقت ومن أي مكان.',
   applicationName: 'Tinghir TV',
+ keywords: [
+   'tinghir tv',
+    'kora live',
+    'بث مباشر',
+    'مباريات اليوم',
+    'نتائج المباريات',
+    'الدوري الإسباني',
+    'الدوري الإيطالي',
+    'الدوري السعودي',
+    'دوري أبطال أوروبا',
+    'live football',
+  ], // ← هادي كانت ناقصة
+
+   metadataBase: new URL('https://tinghir-tv.vercel.app'),
+ verification: {
+    google: 'VzK0Ce6V01Jr_8MvkE_i_rwtYftEtrnNdtl5KY1cIdQ',
+  },
+  robots: {
+  index: true,
+  follow: true,
+},
+  alternates: {
+    canonical: '/',
+  },
+  openGraph: {
+    title: 'tinghir tv',
+    description: 'تابع مباريات اليوم بث مباشر.',
+    url: 'https://tinghir-tv.vercel.app',
+    siteName: 'tinghir tv',
+    locale: 'ar_MA',
+    type: 'website',
+      images: [
+    {
+      url: '/TinghirTV.png',
+      width: 1024,
+      height: 1024,
+      alt: 'tinghir tv',
+    },
+  ],
+  },
+
+  twitter: {
+    card: 'summary_large_image',
+    title: 'tinghir tv',
+    description: 'تابع مباريات اليوم بث مباشر.',
+  },
+  
   icons: {
     icon: 'data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="%23ff1a44" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="12" cy="12" r="10"></circle><path d="M12 2v20M2 12h20m-3-7-14 14m0-14 14 14"/></svg>',
   },
