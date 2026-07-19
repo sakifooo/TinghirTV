be49d96f6db0529db6e2a25d0590f9c28879e10f
sakifooo <saki.sasin@gmail.com>
2026-07-19 17:09:54 +0100

    Update robots.ts

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: '*',
       allow: '/',
     },
-    sitemap: 'https://tinghir-tv.vercel.app/sitemap.xml',
+    sitemap: 'https://tinghirtv.site/sitemap.xml',
   }
 }
