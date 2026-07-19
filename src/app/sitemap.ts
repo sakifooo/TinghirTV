ef67e8ef0a79fcb8949fb6ce82b412dff6e179d3
sakifooo <saki.sasin@gmail.com>
2026-07-19 17:10:15 +0100

    Update sitemap.ts

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,7 +3,7 @@ import { MetadataRoute } from 'next'
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://tinghir-tv.vercel.app',
+      url: 'https://tinghirtv.site/',
       lastModified: new Date(),
       changeFrequency: 'daily',
       priority: 1,
